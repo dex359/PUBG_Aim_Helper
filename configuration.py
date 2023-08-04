b0a824fa53565d11ab589e87b8f3d5338f594068
Denys Ksenchuk <denny.ks359@gmail.com>
2023-08-04 22:20:19 +0300

    Checking and launching Steam and game

--- configuration.py
+++ configuration.py
@@ -1,9 +1,21 @@
+# File containing constants that configure the script's operation,
+# intended for manual editing in an editor.
 
-
+# Path to the executable file of the Steam application store differs for different users
 STEAM_LOCATION = r"S:\Software\Steam\steam.exe"
+
+# PUBG: BATTLEGROUNDS Steam AppID
+# It may change over time, and if there is an issue with an incorrect identifier,
+# it should be corrected when unable to launch game using the script.
 PUBG_ID = 578080
+
+# List of Windows processes, presence of which in the list of running processes
+# indicates correct operation of game.
 GAME_PROCESSES = ["TslGame.exe",
                   "TslGame.exe",
                   "ExecPubg.exe",
                   "zksvc.exe"]
+
+# Maximum waiting time, after which, in case any of specified game processes are absent,
+# game will be considered as not working correctly or not launched.
 WAITING_FOR_LAUNCH = 30
--- main.py
+++ main.py
@@ -7,35 +7,58 @@ import psutil
 import helpers
 import configuration as cfg
 
-### ---------------------------------------- ###
 
-print(" --- SERVICE STARTED --- ")
+class KeyPressListener:
 
-# check Steam process
-if helpers.get_active_processes(["Steam.exe"]):
-    print("Steam process has been found.")
-else:
-    print("Steam process NOT DETECTED!\nLaunching Steam...")
-    helpers.launch_process(cfg.STEAM_LOCATION)
-    print("Steam launched.")
-# check PUBG status
-if helpers.check_game_status(cfg.GAME_PROCESSES):
-    game = helpers.get_main_process(cfg.GAME_PROCESSES)
-    print("Game process has been found.")
-else:
-    print("Game process NOT DETECTED!")
-    helpers.launch_process(f"{cfg.STEAM_LOCATION} -applaunch {cfg.PUBG_ID}")
-    helpers.countdown("Launching game",
-                      cfg.WAITING_FOR_LAUNCH,
-                      lambda: helpers.check_game_status(cfg.GAME_PROCESSES))
-    game = helpers.get_main_process(cfg.GAME_PROCESSES)
-    if game is None:
-        helpers.countdown("Error: unable to start the game! Terminate in", 5, shutdown=True)
+    def __init__(self):
+        pass
+
+
+class Server:
+
+    def __init__(self):
+        pass
+
+    def loop_event(self):
+        pass
+
+
+def run():
+
+    print(" --- SERVICE STARTED --- ")
+
+    # check Steam process
+    if helpers.get_active_processes(["Steam.exe"]):
+        print("Steam process has been found.")
     else:
-        print("Game launched.")
+        print("Steam process NOT DETECTED!\nLaunching Steam...")
+        helpers.launch_process(cfg.STEAM_LOCATION)
+        print("Steam launched.")
 
-while game.is_running():
-    print("cycle")
-    time.sleep(1)
-else:
-    helpers.countdown("Game process terminated. Shutdown at", 3, shutdown=True)
+    # check PUBG status
+    if helpers.check_game_status(cfg.GAME_PROCESSES):
+        game = helpers.get_main_process(cfg.GAME_PROCESSES)
+        print("Game process has been found.")
+    else:
+        print("Game process NOT DETECTED!")
+        helpers.launch_process(f"{cfg.STEAM_LOCATION} -applaunch {cfg.PUBG_ID}")
+        helpers.countdown("Launching game",
+                          cfg.WAITING_FOR_LAUNCH,
+                          lambda: helpers.check_game_status(cfg.GAME_PROCESSES))
+        game = helpers.get_main_process(cfg.GAME_PROCESSES)
+        if game is None:
+            helpers.countdown("Error: unable to start the game! Terminate in", 5, shutdown=True)
+        else:
+            print("Game launched.")
+
+    # handle main loop
+    server = Server()
+
+    while game.is_running():
+        server.loop_event()
+    else:
+        helpers.countdown("Game process terminated. Shutdown at", 3, shutdown=True)
+
+
+if __name__ == "__main__":
+    run()
